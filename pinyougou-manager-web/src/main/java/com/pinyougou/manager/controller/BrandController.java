b6ab9bf5bb1e353ea2474edcefdb66168e32b2ba
KK <1813916568@qq.com>
2018-12-12 19:39:56 +0800

    提交brandservice

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -2,6 +2,7 @@ package com.pinyougou.manager.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
 
 import entity.PageResult;
+import entity.Result;
 
 @RestController
 @RequestMapping("/brand")
@@ -25,5 +27,17 @@ public class BrandController {
 	@RequestMapping("/findPage")
 	public PageResult findPage(int page, int size) {
 		return brandService.findPage(page, size);
+	}
+	@RequestMapping("/add")
+	public Result add(@RequestBody TbBrand tbBrand) {
+		try {
+			brandService.add(tbBrand);
+			return new Result(true, "添加成功");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return new Result(false, "添加失败");
+		}
+		
 	}
 }
