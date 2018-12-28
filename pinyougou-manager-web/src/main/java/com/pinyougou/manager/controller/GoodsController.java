0e6a45f43fbf373f41e06dbca6e25a58187918d4
KK <1813916568@qq.com>
2018-12-28 23:51:11 +0800

    网页静态化技术Freemarker

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/GoodsController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/GoodsController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.page.service.ItemPageService;
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.pojo.TbItem;
 import com.pinyougou.pojogroup.Goods;
@@ -27,6 +28,8 @@ public class GoodsController {
 	private GoodsService goodsService;
 	@Reference
 	private ItemSearchService itemSearchService;
+	@Reference(timeout=50000)
+	private ItemPageService itemPageService;
 	
 	/**
 	 * 返回全部列表
@@ -121,12 +124,20 @@ public class GoodsController {
 				}else {
 					System.out.println("没有明细数据");
 				}
+				//静态页面生成
+				for (Long goodsId : ids) {
+					itemPageService.genItemHtml(goodsId);
+				}
 			}
 			return new Result(true, "成功");
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new Result(false, "失败");
 		}		
+	}
+	@RequestMapping("/genHtml")
+	public void genHtml(Long goodsId) {
+		itemPageService.genItemHtml(goodsId);
 	}
 	
 }
